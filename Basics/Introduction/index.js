88884e2d2120881dc1b4508615dad04041235970
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-11-23 15:45:34 +0530

    test

---
+++ Basics/Introduction/index.js
@@ -0,0 +1,37 @@
+// alert("Welcome to js from extrenal file")
+console.log("Hey there")
+console.log("1234567892010202")
+console.log(5+5)
+console.log(10-2)
+
+console.log(2.5)
+
+console.log(true)
+
+console.log([10,20,2.5])
+console.log({id:'sarathy'})
+
+
+// console.table
+console.table({
+    id : 100,
+    u_name : "Sarathy",
+    address: "Bangalore"
+})
+
+// console.error & console.warn
+
+console.error("Something is wrong")
+
+console.warn("Warning")
+
+
+// console.clear()
+
+console.time("Timer")
+
+for (i=0;i<=150;i++){
+    console.log(i)
+}
+
+console.timeEnd("Timer")
---
+++ Basics/Variable/first.js
@@ -0,0 +1,56 @@
+/*
+var
+let 
+const
+*/ 
+
+// Difference
+
+// 1.Scope
+
+// var -can accessed anywhere inside the scope or outside the scope
+// let - can be accessed within the scope
+// const - its same as let
+if(true){
+    // var msg = "Hello"
+    let msg = "Hello"
+    // const msg = "Hello"
+
+    console.log(msg)
+}
+
+// console.log(msg)
+
+
+// 2.Variable Declaration
+/* 
+var- redeclaration is allowed
+let - redeclaration is not allowed
+const - redeclaration is not allowed 
+*/
+var a = 25;
+console.log(a)
+var a = 40
+console.log(a)
+
+// 3.Value Assignment
+
+// var,let - We can re assign the value
+// const- cannot be reassigned
+
+var a= 20
+console.log(a)
+a = 40
+console.log(a)
+
+let b = 10
+console.log(b)
+b= 12
+console.log(b)
+
+// if we want to change the const value
+// we can't change the value of normal variable but we can change the object of const value
+
+const student = {"name":"Sarathy","age":18}
+student.name = "Shafna"
+console.table(student)
