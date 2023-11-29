1713967321ef89c15c530395094cfe204e29cfb3
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-11-29 12:55:55 +0530

    tst

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom"
+// import { render } from "@testing-library/react"
+ReactDOM.render("Hello,Good Morning",document.getElementById("something"))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render("Hey, It's First Div element",document.getElementById("one"))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render("Hey there, It's my second div",document.getElementById("two"))
