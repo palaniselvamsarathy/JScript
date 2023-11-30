8fc4022f4fdbf8d864e77c02ca0822c9503a02d0
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-11-30 15:50:12 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,11 @@
-import Navbar from './Navbar/Navbar'
-import Message  from './Message/Message'
-import Msg from './Message/Msg'
-import Msgobject from './Message/Msgobject'
+
 
 let App =()=>{
     return <>
-        < Navbar/>
+    
         <h1>Hey there</h1>
         <h2>How's your day?</h2>
-        <Message/>
-        <Msg/>
-        <Msgobject/>
     </>
 }
 
-export default App
+export default App 
