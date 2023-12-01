292863075b878c3a7d1e245b1f66045cba64576f
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-01 18:29:55 +0530

    tst

---
+++ my-app/Backup/Props/Example with array/src/App.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar/Navbar'
+import Employee from './Employee/Employee'
+let App =()=>{
+    return <>
+        <Navbar/>
+        <Employee/>
+    </>
+}
+
+export default App
