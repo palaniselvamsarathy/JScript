9d710d9181ba873327b1f8db8f6a144f51150945
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-22 15:15:30 +0530

    okie

---
+++ my-app/Backup/Hooks1/useref/Component/HookTimer.js
@@ -0,0 +1,23 @@
+import React ,{useState,useRef,useEffect} from 'react'
+
+function HookTimer() {
+    const [timer,setTimer]= useState(0)
+    const intervalRef = useRef()
+    useEffect(()=>{
+        intervalRef.current =setInterval(()=>{
+            setTimer(prevTimer=> prevTimer+1)
+        },1000)
+        return()=>{
+            clearInterval(intervalRef.current)
+        };
+    },[])
+   
+  return (
+    <div>
+      Hook Timer -{timer}
+      <button onClick={()=>clearInterval(intervalRef.current)}>Clear</button>
+    </div>
+  )
+}
+
+export default HookTimer
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Counter from './Components/Counter'
 function App() {
   return (
     <div>
       <h1>App Component</h1>
-    <Counter/>
+      
     </div>
   )
 }
