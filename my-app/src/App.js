df1d285357a8deb73befc9b453e0930eac6ce32a
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2023-12-22 11:30:20 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
-
+// import parentComponent from './Components/parentComponent'
+// import parentComponent from './Components/ParentComponent'
+import ParentComponent from './Components/ParentComponent'
 function App() {
   return (
     <div>
       <h1>App Component</h1>
-      
-
-
-
-      
+      <ParentComponent/>
+      {/* <parentComponent/> */}
     </div>
   )
 }
